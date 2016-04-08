618129f6a95915aaaaa1d09a835eb862b5a398d8
Andrew Lauritzen <andrew.lauritzen@gmail.com>
2016-04-08 14:49:31 -0700

    Some more iteration and testing
    Try to simplify the potential paths

--- source/app.d
+++ source/app.d
@@ -4,7 +4,7 @@ import std.stdio;
 shared static this()
 {
     auto router = new URLRouter;
-    router.any("*", proxyRequest());
+    router.any("*", proxy_request());
 
 	auto settings = new HTTPServerSettings;
 	settings.port = 80;
@@ -16,15 +16,53 @@ shared static this()
 static void pretty_print_headers(InetHeaderMap headers)
 {
     foreach (k, v ; headers)
-    {
         writefln("  %s: %s", k, v);
-    }
 }
 
-/**
-Returns a HTTP request handler that forwards any request to the specified host/port.
-*/
-HTTPServerRequestDelegateS proxyRequest()
+
+void setup_upstream_request(scope HTTPServerRequest req, scope HTTPClientRequest upstream_req)
+{
+    writefln("%s %s", req.method, req.requestURL);
+    //pretty_print_headers(req.headers);
+
+    // TODO: Any other headers we want to manipulate or not copy?
+    upstream_req.method = req.method;
+    upstream_req.headers = req.headers.dup;
+    upstream_req.headers["Connection"] = "keep-alive";
+
+    // Avoid compressed requests as this path is potentially a bit buggier in vibe.d currently
+    upstream_req.headers.removeAll("Accept-Encoding");
+
+    // Add standard proxy headers
+    if (auto pri = "X-Real-IP" !in upstream_req.headers)
+        upstream_req.headers["X-Real-IP"] = req.clientAddress.toAddressString();
+    if (auto pfh = "X-Forwarded-Host" !in upstream_req.headers) 
+        upstream_req.headers["X-Forwarded-Host"] = req.headers["Host"];
+    if (auto pfp = "X-Forwarded-Proto" !in upstream_req.headers)
+        upstream_req.headers["X-Forwarded-Proto"] = req.tls ? "https" : "http";
+    if (auto pff = "X-Forwarded-For" in req.headers)
+        upstream_req.headers["X-Forwarded-For"] = *pff ~ ", " ~ req.peer;
+    else
+        upstream_req.headers["X-Forwarded-For"] = req.peer;
+
+    // If they provide a content length, use it
+    if ("Content-Length" in req.headers)
+        upstream_req.writeBody(req.bodyReader, upstream_req.headers["Content-Length"].to!size_t());
+    else if (!req.bodyReader.empty)
+    {
+        // Chunked encoding... note that Steam servers don't seem too happy with this generally which
+        // is why we try to avoid this path when proxying.
+        upstream_req.writeBody(req.bodyReader);
+    }
+    // Otherwise don't write any body
+
+    //writeln("PROXY REQUEST HEADERS:");
+    //pretty_print_headers(upstream_req.headers);
+    //writeln();
+}
+
+
+HTTPServerRequestDelegateS proxy_request()
 {
 	URL url;
 	url.schema = "http";
@@ -38,107 +76,68 @@ HTTPServerRequestDelegateS proxyRequest()
 
         // TODO: Decide whether to use cached response
 
-		void setupClientRequest(scope HTTPClientRequest creq)
+        void upstream_request(scope HTTPClientRequest upstream_req)
+        {
+            setup_upstream_request(req, upstream_req);
+        }
+
+		void upstream_response(scope HTTPClientResponse upstream_res)
 		{
-            writeln("REQUEST:");
-            writefln("%s %s", req.method, req.requestURL);
-            pretty_print_headers(req.headers);
-
-            // TODO: Any other headers we want to manipulate or not copy?
-			creq.method = req.method;
-			creq.headers = req.headers.dup;
-			creq.headers["Connection"] = "keep-alive";
-
-            // if (auto pfh = "X-Real-IP" !in creq.headers) creq.headers["X-Real-IP"] = creq.clientAddress.toAddressString();
-			if (auto pfh = "X-Forwarded-Host" !in creq.headers) creq.headers["X-Forwarded-Host"] = req.headers["Host"];
-			if (auto pfp = "X-Forwarded-Proto" !in creq.headers) creq.headers["X-Forwarded-Proto"] = req.tls ? "https" : "http";
-			if (auto pff = "X-Forwarded-For" in req.headers) creq.headers["X-Forwarded-For"] = *pff ~ ", " ~ req.peer;
-			else creq.headers["X-Forwarded-For"] = req.peer;
-            
-            // If they provide a content length, use it
-            if ("Content-Length" in req.headers)
-                creq.writeBody(req.bodyReader, req.headers["Content-Length"].to!size_t());
-            else if (!req.bodyReader.empty)
-            {
-                // Chunked encoding... note that Steam servers don't seem too happy with this generally
-                writeln("CHUNKED ENCODING, OH OH!");
-                creq.writeBody(req.bodyReader);
-            }
-            
-            //writeln("PROXY REQUEST HEADERS:");
-            //pretty_print_headers(creq.headers);
-            //writeln();
-		}
-
-		void handleClientResponse(scope HTTPClientResponse cres)
-		{
-			import vibe.utils.string;
-
             // We don't currently support HTTP/1.0 clients
 			if (res.httpVersion == HTTPVersion.HTTP_1_0) {
                 throw new HTTPStatusException(HTTPStatus.httpVersionNotSupported);
 			}
 
-            writeln("ORIGINAL RESPONSE:");
-            pretty_print_headers(cres.headers);
-
-            // TODO: Decide whether to cache this response
+            writefln("UPSTREAM RESPONSE: %s", upstream_res.statusCode);
+            //pretty_print_headers(cres.headers);
 
             // Copy relevant response headers
-            res.statusCode = cres.statusCode;
-			foreach (key, value; cres.headers) {
-                // TODO: Other headers not to keep?
-				if (icmp2(key, "Connection") != 0)
-					res.headers[key] = value;
-			}
+            res.statusCode = upstream_res.statusCode;
+            foreach (key, value; upstream_res.headers) {
+                // TODO: Remove any other fields? Transfer-Encoding?
+                // Some will just naturally get overwritten when we write the body
+                if (icmp2(key, "Connection") != 0)
+                    res.headers[key] = value;
+            }
+
+            if (res.isHeadResponse) {
+                res.writeVoidBody();
+                return;
+            }
 
             // If content length is specified, perform an almost raw copy of the response
-			if ("Content-Length" in cres.headers) {
-                auto bodySize = cres.headers["Content-Length"].to!size_t();
-
-				if (res.isHeadResponse)
-                    res.writeVoidBody();
-                else if (bodySize == 0)
+			if ("Content-Length" in upstream_res.headers)
+            {
+                auto bodySize = upstream_res.headers["Content-Length"].to!size_t();
+				
+                if (bodySize == 0)
                     res.writeBody(cast(ubyte[])"", null);
 				else
                 {
-                    cres.readRawBody((scope reader) {
-                        res.writeRawBody(reader, bodySize);
-                    });
+                    // TODO: Decide whether to cache this response
+
+                    auto payload = upstream_res.bodyReader.readAll();
+                    res.writeBody(payload);
+
+                    //cres.readRawBody((scope reader) {
+                    //    res.writeRawBody(reader, bodySize);
+                    //});
                 }
 				assert(res.headerWritten);
 
-                writeln("PROXY RESPONSE HEADERS:");
-                pretty_print_headers(res.headers);
-                writeln();
+                //writeln("PROXY RESPONSE HEADERS:");
+                //pretty_print_headers(res.headers);
+                //writeln();
 
 				return;
 			}
-
-            // No content length in headers
-
-			// Handle empty response bodies
-			if (req.method == HTTPMethod.HEAD || "Transfer-Encoding" !in cres.headers) {
-				res.writeVoidBody();
-                return;
-			}
 			
             writeln("NOT IMPLEMENTED");
-            throw new HTTPStatusException(HTTPStatus.notImplemented);
-
-            /*
-			// fall back to a generic re-encoding of the response
-			// copy all headers that may pass from upstream to client
-			foreach (n, v; cres.headers) {
-				if (n !in non_forward_headers_map)
-					res.headers[n] = v;
-			}
-			if (res.isHeadResponse) res.writeVoidBody();
-			else res.bodyWriter.write(cres.bodyReader);
-            */
+            assert(false);
+            //throw new HTTPStatusException(HTTPStatus.notImplemented);
 		}
 
-		requestHTTP(rurl, &setupClientRequest, &handleClientResponse);
+		requestHTTP(rurl, &upstream_request, &upstream_response);
 	}
 
 	return &handleRequest;
