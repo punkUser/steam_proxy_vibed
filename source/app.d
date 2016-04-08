d231352bb3436b602f980c2a25e59006c69cfd4f
Andrew Lauritzen <andrew.lauritzen@gmail.com>
2016-04-08 15:25:33 -0700

    More rearranging for caching
    Will likely separate out the cached response into another class or
    similar.

--- source/app.d
+++ source/app.d
@@ -61,7 +61,6 @@ void setup_upstream_request(scope HTTPServerRequest req, scope HTTPClientRequest
     //writeln();
 }
 
-
 HTTPServerRequestDelegateS proxy_request()
 {
 	URL url;
@@ -74,6 +73,11 @@ HTTPServerRequestDelegateS proxy_request()
         rurl.host = req.host;
 		rurl.localURI = req.requestURL;
 
+        // We don't currently support HTTP/1.0 clients
+        if (res.httpVersion == HTTPVersion.HTTP_1_0) {
+            throw new HTTPStatusException(HTTPStatus.httpVersionNotSupported);
+        }
+
         // TODO: Decide whether to use cached response
 
         void upstream_request(scope HTTPClientRequest upstream_req)
@@ -83,13 +87,33 @@ HTTPServerRequestDelegateS proxy_request()
 
 		void upstream_response(scope HTTPClientResponse upstream_res)
 		{
-            // We don't currently support HTTP/1.0 clients
-			if (res.httpVersion == HTTPVersion.HTTP_1_0) {
-                throw new HTTPStatusException(HTTPStatus.httpVersionNotSupported);
-			}
-
-            writefln("UPSTREAM RESPONSE: %s", upstream_res.statusCode);
+            writefln("UPSTREAM RESPONSE: %s", upstream_res.toString());
             //pretty_print_headers(cres.headers);
+
+            // TODO: Decide whether to cache this response
+            // For consistency, should we just always take the same path here?
+            // i.e. do the request and cache it, then read it from the cache to satisfy the original
+            // request? In theory the file cache should handle this reasonably well. If not we can
+            // add some logic to the caching layer, but the advantage is guaranteed same behavior
+            // for the "first" client that requests something and later ones that hit the cache.
+            // That said, we also want as much consistency between the cached and not cached
+            // request paths as possible, so there's no perfect answer.
+            // NOTE: We choose the cache the upstream response rather than the modified response
+            // that we send to the client here. This means slightly more overhead as even cache
+            // hits have to run through the logic below again, but it means that the data in the
+            // cache is far less coupled to any logic changes in the proxy code. If this code
+            // eventually settings down and/or CPU overhead becomes an issue here, it's easy enough
+            // to change.
+
+            //auto cached_response = Bson([
+                //"response": (cast(HTTPResponse)upstream_res).serializeToJson,
+                //"field2": Bson(42),);
+
+            auto cached_response = Json([
+                "response": (cast(HTTPResponse)upstream_res).serializeToJson()
+            ]);
+            writeln(cached_response.toPrettyString());
+
 
             // Copy relevant response headers
             res.statusCode = upstream_res.statusCode;
@@ -114,10 +138,9 @@ HTTPServerRequestDelegateS proxy_request()
                     res.writeBody(cast(ubyte[])"", null);
 				else
                 {
-                    // TODO: Decide whether to cache this response
-
                     auto payload = upstream_res.bodyReader.readAll();
                     res.writeBody(payload);
+
 
                     //cres.readRawBody((scope reader) {
                     //    res.writeRawBody(reader, bodySize);
